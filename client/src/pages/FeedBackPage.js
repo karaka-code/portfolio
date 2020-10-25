ce08d81739342357e8be47a6c61109d2aa20ed9e
coole133 <vlad.yaromiy@gmail.com>
2020-10-25 18:48:18 +0200

    sort by date

--- client/src/pages/FeedBackPage.js
+++ client/src/pages/FeedBackPage.js
@@ -1,84 +1,92 @@
-import React, {useEffect, useState} from "react";
-import {Button, Form} from "react-bootstrap";
-import axios from "axios"
-import {useSelector} from "react-redux";
-import {Modal} from "react-bootstrap";
-import "./FeedBackPage.css"
-
+import React, { useEffect, useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { Modal } from "react-bootstrap";
+import "./FeedBackPage.css";
 
 const FeedBackPage = () => {
-    const [text, setText] = useState('')
-    const [feedbacks, setFeedbacks] = useState([])
-    const [show, setShow] = useState(false);
+  const [text, setText] = useState("");
+  const [feedbacks, setFeedbacks] = useState([]);
+  const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const user = useSelector(state => state.user.currentUser)
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const user = useSelector((state) => state.user.currentUser);
 
-    useEffect(() => {
-        axios.get("api/feedback/")
-            .then((response) => {
-                setFeedbacks(response.data)
-            })
-    }, [])
+  useEffect(() => {
+    axios.get("api/feedback/").then((response) => {
+      setFeedbacks(response.data);
+    });
+  }, []);
 
-    const sendFeedback = (e) => {
-        e.preventDefault()
-        if (user === null) {
-            handleShow()
-            return
-        }
-        axios.post("api/feedback/", {
-            text, userId: user.userId, name: user.name
-        })
-            .then((response) => {
-                console.log(response)
-            }, (error) => {
-                console.log(error)
-            })
-        setText('')
+  const sendFeedback = (e) => {
+    e.preventDefault();
+    if (user === null) {
+      handleShow();
+      return;
     }
+    axios
+      .post("api/feedback/", {
+        text,
+        userId: user.userId,
+        name: user.name,
+      })
+      .then(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    setText("");
+  };
 
-    return (
-        <div className="container">
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Body>You need to login to leave feedbacks</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="primary" onClick={handleClose}>
-                        Close
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-            <Form className="form">
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Leave your feedback here.</Form.Label>
-                    <Form.Control
-                        as="textarea"
-                        value={text}
-                        onChange={(e) => setText(e.target.value)}
-                        rows={10}
-                    />
-                </Form.Group>
-                <Button variant="primary" type="submit" onClick={sendFeedback}>
-                    Leave
-                </Button>
-            </Form>
-            <div className="feedbacks">
-                <p>Feedbacks: </p>
-                <div>
-                    {feedbacks.length !== 0 ? feedbacks.map(item => {
-                        return (
-                            <div key={item._id} className="feedback">
-                                <p>User: {item.name}</p>
-                                <p>Text: {item.text}</p>
-                                <p className="time">{item.time}</p>
-                            </div>
-                        )
-                    }) : <p>No feedbacks</p>}
+  return (
+    <div className="container">
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Body>You need to login to leave feedbacks</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Form className="form">
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Leave your feedback here.</Form.Label>
+          <Form.Control
+            as="textarea"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            rows={10}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={sendFeedback}>
+          Leave
+        </Button>
+      </Form>
+      <div className="feedbacks">
+        <p>Feedbacks: </p>
+        <div style={{ overflow: "scroll" }}>
+          {feedbacks.length !== 0 ? (
+            feedbacks.map((item) => {
+              return (
+                <div key={item._id} className="feedback">
+                  <p>User: {item.name}</p>
+                  <p>Text: {item.text}</p>
+                  <p className="time">{item.time}</p>
                 </div>
-            </div>
+              );
+            })
+          ) : (
+            <p>No feedbacks</p>
+          )}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default FeedBackPage
+export default FeedBackPage;
