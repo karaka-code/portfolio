3f970277355182fe437ab61dfe1f40caefd26c8e
asce-del <vlad.yaromiy@gmail.com>
2020-11-01 19:49:38 +0200

    as

--- client/src/components/LiveChat.js
+++ client/src/components/LiveChat.js
@@ -9,7 +9,7 @@ import ChatCard from "./ChatCard";
 import AlertModal from "./AlertModal";
 
 let socket;
-const server = 'http://localhost:5000' || process.env.HOST;
+const server = process.env.HOST || 'http://localhost:5000';
 
 
 const LiveChat = () => {
