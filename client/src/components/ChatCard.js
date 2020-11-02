2e8a9edad92e4af30b7dc39f9f0fdeae44ec63fb
asce-del <vlad.yaromiy@gmail.com>
2020-11-03 00:25:40 +0200

    time

--- client/src/components/ChatCard.js
+++ client/src/components/ChatCard.js
@@ -6,10 +6,12 @@ const ChatCard = ({chat}) => {
     return (
         <>
             {chat
-                ? <div className="message">
-                    <p style={{margin: 0, textAlign: "end"}}>{chat.sender.name}</p>
-                    <p className="text">{chat.message}</p>
-                    <p className="time">{chat.time}</p>
+                ? <div className="msg-container">
+                    <p className="msg-time">{chat.time}</p>
+                    <div className="message">
+                        <p style={{margin: 0, textAlign: "end"}}>{chat.sender.name}</p>
+                        <p className="text">{chat.message}</p>
+                    </div>
                 </div>
                 : null
             }
